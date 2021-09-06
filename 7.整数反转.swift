aab6f64790e91730d49f063081749062103b5bf6
ponng <ponng@outlook.com>
2021-09-06 13:39:08 +0800

    增加问题解法: [7] 整数反转 第二个解法

--- 7.整数反转.swift
+++ 7.整数反转.swift
@@ -7,28 +7,41 @@
 // @lc code=start
 class Solution {
     func reverse(_ x: Int) -> Int {
-        let s:String = String(x)
-        var sArr = s.compactMap { char in
-            String(char)
+        // 解法2: 转字符串并倒叙
+        // let s:String = String(x)
+        // var sArr = s.compactMap { char in
+        //     String(char)
+        // }
+        
+        // let isNegative = sArr.first == "-"
+        
+        // if isNegative{
+        //     sArr[0] = ""
+        // }
+        // sArr = sArr.reversed()
+        
+        // var resultStr = sArr.joined()
+        
+        // if Double(resultStr)! > pow(2, 31) - 1 {
+        //     return 0
+        // }
+        
+        // if isNegative{
+        //     resultStr = "-" + resultStr
+        // }
+        // return Int(resultStr)!
+
+        // 解法2：循环取余数
+        var result = 0
+        var num = x
+        while num != 0 {
+            result = result * 10 + num % 10
+            num = num/10
         }
-        
-        let isNegative = sArr.first == "-"
-        
-        if isNegative{
-            sArr[0] = ""
-        }
-        sArr = sArr.reversed()
-        
-        var resultStr = sArr.joined()
-        
-        if Double(resultStr)! > pow(2, 31) - 1 {
+        if result < Int32.min || result > Int32.max {
             return 0
         }
-        
-        if isNegative{
-            resultStr = "-" + resultStr
-        }
-        return Int(resultStr)!
+        return result
     }
 }
 // @lc code=end
